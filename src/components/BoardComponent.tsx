c2eaf3bc84dfc1fcc61f760706526de078d91612
hot-play <usov2002@yandex.ru>
2022-07-11 13:53:15 +0700

    init classes

---
+++ src/components/BoardComponent.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Board } from "../models/Board";
+
+interface BoardProps {
+    board:Board;
+    setBoard: (board: Board)=> void;
+}
+
+const BoardComponent: FC<BoardProps> = ({board, setBoard}) =>{
+    return(
+        <div
+        className="board"
+        >
+        </div>
+    )
+}
+export default BoardComponent;
